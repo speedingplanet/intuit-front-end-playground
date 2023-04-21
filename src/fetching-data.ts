c2da05c3969aca274adec1dd946011a92a56483e
John Paxton <johnpaxton@gmail.com>
2023-04-21 14:40:09 -0400

    Add clear output button

--- src/fetching-data.ts
+++ src/fetching-data.ts
@@ -1,7 +1,9 @@
 let fetchPromisesButton = document.querySelector('#button-fetch-promises');
 let fetchAsyncButton = document.querySelector('#button-fetch-async');
+let clearOutputButton = document.querySelector('#button-clear-output');
 let outputDiv = document.querySelector('#output');
 
+clearOutputButton?.addEventListener('click', () => outputDiv?.replaceChildren());
 fetchPromisesButton?.addEventListener('click', fetchWithPromises);
 fetchAsyncButton?.addEventListener('click', fetchWithAsync);
 
